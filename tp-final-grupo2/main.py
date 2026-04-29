8189ca98a88ba4a4bc9d01fba1edc473349dce80
Lautaro F <160680628+Learnerwanxiety@users.noreply.github.com>
2026-04-29 16:08:21 -0300

    Update main.py
    Agregué el menú arriba para que el inicio del While no se note

--- tp-final-grupo2/main.py
+++ tp-final-grupo2/main.py
@@ -28,7 +28,45 @@ while len(str(dniCliente))>15:
     print("ERROR-- El numero de telefono no puede tener más de 15 digitos, vuelva a ingresar. ")
     numTelCliente=int(input("Ingrese su numero de teléfono: "))
 
-ciclo=int(input("Ingrese un numero>0 para iniciar el sistema: "))
+   print("------MENÚ------")
+    print("""
+    ROYAL PAWS
+      
+    SERVICIOS
+LAVADO: $6.000
+CORTE: $5.000
+FULL: $10.000
+
+    PRODUCTOS
+JUGUETE SQUEAKY: $2.500
+SHAMPOO: $3.000
+     COMUN 
+        SHAMPOO 2 EN 1
+     SHAMPOO PARA PIELES ATOPICAS
+      
+CHALECOS DE ABRIGO PERRO( XS, S, M, L, XL ): $5.000
+      
+    DESCUENTOS Y PROMOCIONES
+FULL + JUGUETE SQUEAKY: 15%
+LAVADO + CHALECO: 8%
+UNA COMPRA MAYOR A $10.000 ENVIO GRATIS
+      
+    RECARGOS
+RAZA PELIGROSA: %8
+MASCOTA EN MAL ESTADO: %20  
+
+    
+      ENVIOS
+ENVIOS A CABA: $1500
+ENVIOS A Prov. BUENOS AIRES: $8000
+
+    MEDIOS DE PAGO
+TARJETA DE CREDITO/DEBITO
+EFECTIVO
+TRANSFERIENCIA 
+      
+      """)
+ciclo=int(input("La opción que desee: "))
 
 while ciclo>0: 
     
